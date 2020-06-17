077737108a1707b6e69ac46ae707ebb7b3dc8b79
GeorgeCoops <coopsgeorge@gmail.com>
2020-06-17 18:11:17 +0100

    GC - Post destroy bug sort

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    has_many :post_images
+    has_many :post_images, dependent: :destroy
     validates :position, :company, :location, :instagram, :facebook, :instagram, :length, :description, presence: true
 end
