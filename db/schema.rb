ae4d4a17d8ffbb6acacd989344b829b76e3cf333
GeorgeCoops <coopsgeorge@gmail.com>
2020-06-18 17:58:34 +0100

    GC - after presentation

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_06_06_122344) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "post_images", force: :cascade do |t|
     t.string "url"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_post_images_on_post_id"
